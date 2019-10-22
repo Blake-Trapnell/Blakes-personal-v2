9cfcd5dc10dfe13c4fb92b52b2f5e139b5ef321f
Blake-Trapnell <52056662+Blake-Trapnell@users.noreply.github.com>
2019-10-21 21:09:38 -0600

    skills links added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
             <svg>
                 <filter id="water" x="0" y="0" width="100%" height="100%">
                     <feTurbulence id="water-filter" numOctaves="5" seed="4" baseFrequency="0.1 0.01"> </feTurbulence>
-                    <feDisplacementMap scale="7" in="SourceGraphic"></feDisplacementMap>
+                    <feDisplacementMap scale="8" in="SourceGraphic"></feDisplacementMap>
                     <animate xlinkHref="#water-filter" attributeName="baseFrequency" dur="60s"
                     keytime="0;0.5" values="0.04 0.08; 0.05 0.09; 0.04 0.08" repeatCount = "indefinite"></animate>
                 </filter>
--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -1,60 +1,123 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import "./Information.css"
 
 export default class Information extends Component {
-    state={
-        displayContact: false,
-        displaySkills: false,
-        displayProjecs: false
+    state = {
+        displayContact: 0,
+        displaySkills: 0,
+        displayProjects: 0,
     }
 
     changeDisplay(key, e) {
-        this.setState({
-            [key]: !e
-        })
+        if (e === 0) {
+            this.setState({
+                [key]: 1
+            })
+        }
+        else {
+            this.setState({
+                [key]: 0
+            })
+        }
+
     }
 
-render() {
+    render() {
 
-    return (
-        <div className="information-outer">
-            <div className="left">
-            {this.state.displayContact === true ? 
-            <div className="Contact_Outer">
-                <img src={require("../../Pics/profile.jpg")} className="Contact_Profile_image"></img>
+        return (
+            <div className="information-outer">
+                <div style={{ opacity: this.state.displayContact }} className="left">
+                    <div className="Contact_Outer">
+                        <img src={require("../../Pics/profile.jpg")} className="Contact_Profile_image"></img>
 
-                    <h3 id="email" className="Contact_Links email">Email: <p>
-                        BlakeTrapnell.Dev@Gmail.com</p>
-                    </h3>
-                    <a style={{ color: "whitesmoke"}} target="_blank" href="https://github.com/Blake-Trapnell" >
-                        <h3 id="github" className="Contact_Links github">GitProfile: <p>
-                            https://github.com/Blake-Trapnell</p>
+                        <h3 id="email" className="Contact_Links email">Email: <p>
+                            BlakeTrapnell.Dev@Gmail.com</p>
                         </h3>
-                    </a>
-                    <a style={{ color: "whitesmoke"}} target="_blank" href="https://www.linkedin.com/in/blake-trapnell-0b526495/">
-                        <h3 id="linkedin" className="Contact_Links linkedin">LinkedIn Profile: https://www.linkedin.com/in/blake-trapnell-0b526495/</h3>
-                    </a>
-                   
-                        <h1 id="aboutme" className="Contact_Links Aboutme">About me</h1>
-            </div>
-                        : null}
-                   
-            </div> 
-            <div className="center">
-                <div className="center-outer">
+                        <a style={{ color: "whitesmoke" }} target="_blank" href="https://github.com/Blake-Trapnell" >
+                            <h3 id="github" className="Contact_Links github">GitProfile: <p>
+                                https://github.com/Blake-Trapnell</p>
+                            </h3>
+                        </a>
+                        <a style={{ color: "whitesmoke" }} target="_blank" href="https://www.linkedin.com/in/blake-trapnell-0b526495/">
+                            <h3 id="linkedin" className="Contact_Links linkedin">LinkedIn Profile: https://www.linkedin.com/in/blake-trapnell-0b526495/</h3>
+                        </a>
 
-                <h1>Blake Trapnell</h1>
-                <h3 className="smaller-text">Fullstack Web Developer</h3>
-                <div className="display-info">
-                <h3 onClick={()=> this.changeDisplay("displayContact", this.state.displayContact)} className="smaller-text hover">Contact</h3>
-                <h3 onClick={()=> this.changeDisplay("displaySkills", this.state.displayContact)} className="smaller-text hover skills">skills</h3>
-                <h3 onClick={()=> this.changeDisplay("displayProjects", this.state.displayContact)} className="smaller-text hover">Projects</h3>
+                        <h1 id="aboutme" className="Contact_Links Aboutme">About me</h1>
+                    </div>
                 </div>
+                <div className="center">
+                    <div className="center-outer">
+                        <h1>Blake Trapnell</h1>
+                        <h3 className="smaller-text">Fullstack Web Developer</h3>
+                        <div className="display-info">
+                            <h3 onClick={() => this.changeDisplay("displayContact", this.state.displayContact)} className="smaller-text hover">Contact</h3>
+                            <h3 onClick={() => this.changeDisplay("displaySkills", this.state.displaySkills)} className="smaller-text hover skills">skills</h3>
+                            <h3 onClick={() => this.changeDisplay("displayProjects", this.state.displayProjects)} className="smaller-text hover">Projects</h3>
+                        </div>
+                    </div>
+                    <div style={{ opacity: this.state.displaySkills }} className="skills-outer">
+                        <div className="skills-container">
+                            <a className="down"  target="_blank" href="https://www.javascript.com">
+                            <div className="skills-logo js-square"></div>
+                            </a>
+                            <a className="up" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
+                            <div className="skills-logo html5"></div>
+                            </a>
+                            <a className="down" target="_blank" href="https://reactjs.org/">
+                            <div className="skills-logo react"></div>
+                            </a>
+                            <a className="up" target="_blank" href="https://developer.mozilla.org/en-US/docs/Archive/CSS3">
+                            <div className="skills-logo css3"></div>
+                            </a>
+                            <a className="down" target="_blank" href="https://www.adobe.com/products/photoshopfamily.html">
+                            <div className="skills-logo photoshop"></div>
+                            </a>
+                            <a className="up" target ="_blank" href="https://www.adobe.com/products/photoshopfamily.html">
+                            <div className="skills-logo postgres"></div>
+                            </a>
+                            <a className="down" target="_blank" href="https://www.heroku.com/">
+                            <div className="skills-logo heroku"></div>
+                            </a>
+                        </div>
+                        {/* <a target="_blank" href="https://nodejs.org/en/"> */}
+                        <div className="node"></div>
+                        {/* </a> */}
+                    </div>
+                </div>
+                <div className="right">
+                    <div style={{ opacity: this.state.displayProjects }} className="Projects_Outer">
+                        <h1 className="Projects-Title">Projects:</h1>
+                        <div className="Aces-Dungeon-Creator">
+                            <div className="Aces-Background">
+                            </div>
+                        </div>
+                        <div className="About-Aces-Dungeon-Creator">
+                            <a style={{ color: "whitesmoke", fontSize: "1.5rem" }} href="http://www.acesdungeoncreator.com" target="_blank">
+                                <h1 className="Projects-Title">Ace's Dungeon Creator</h1>
+                            </a>
+                            <h4>A mobile responsive website <p>
+                                designed to let users take their</p>
+                                <p>Table Top Dungeons and Dragons</p><p>
+                                    Game on the go with them</p></h4>
+                        </div>
+                        <div className="Game-Traders">
+                            <div className="game-Trader-Background">
+                            </div>
+                        </div>
+                        <div className="About-game-Traders">
+                            <a style={{ color: "whitesmoke", fontSize: "1.5rem" }} href="http://104.248.178.127:4200/#/" target="_blank">
+                                <h1 className="Projects-Title">Game Traders</h1>
+                            </a>
+                            <h4>A Smart rework of the current <p>
+                                pre-owned game Industry</p>
+                                <p>Peer to Peer trades allow</p><p>
+                                    discounted prices on all trades</p></h4>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-            <div className="right"></div>
-        </div>
-    )
-}
+        )
+    }
 }

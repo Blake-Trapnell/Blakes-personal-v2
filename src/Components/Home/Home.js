75e29bcee0460df3c385765efaa68049d5db950d
Blake-Trapnell <52056662+Blake-Trapnell@users.noreply.github.com>
2019-10-22 00:40:52 -0600

    final update for now XD

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,13 +6,21 @@ export default function Home() {
         <div className="background">
             <div className="water">
             </div>
-            <div className="trees"></div>
+            <div className="leaves"></div>
             <svg>
                 <filter id="water" x="0" y="0" width="100%" height="100%">
                     <feTurbulence id="water-filter" numOctaves="5" seed="4" baseFrequency="0.1 0.01"> </feTurbulence>
                     <feDisplacementMap scale="8" in="SourceGraphic"></feDisplacementMap>
                     <animate xlinkHref="#water-filter" attributeName="baseFrequency" dur="60s"
                     keytime="0;0.5" values="0.04 0.08; 0.05 0.09; 0.04 0.08" repeatCount = "indefinite"></animate>
+                </filter>
+            </svg>
+            <svg>
+                <filter id="leaves" x="0" y="0" width="100%" height="100%">
+                    <feTurbulence id="leaves-filter" numOctaves="10" seed="4" baseFrequency="0.02 0.01"> </feTurbulence>
+                    <feDisplacementMap scale="2" in="SourceGraphic"></feDisplacementMap>
+                    <animate xlinkHref="#leaves-filter" attributeName="baseFrequency" dur="60s"
+                    keytime="0;0.5" values="0.04 0.08; 0.06 0.10; 0.04 0.08" repeatCount = "indefinite"></animate>
                 </filter>
             </svg>
         </div>
